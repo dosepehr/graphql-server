682aef8663f1e6c24395b7b63087fef767fea2d3
dosepehr <dosepehr@gmail.com>
2025-02-23 12:08:33 +0330

    add teachers queryt

---
+++ graphql/course.js
@@ -0,0 +1,44 @@
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLSchema,
+    GraphQLList,
+} = require('graphql');
+
+const CourseType = new GraphQLObjectType({
+    name: 'Course',
+    fields: () => ({
+        id: { type: GraphQLString },
+        title: { type: GraphQLString },
+        price: { type: GraphQLString },
+    }),
+});
+const courses = [
+    {
+        id: 1,
+        title: 'js',
+        price: 10,
+    },
+    {
+        id: 1,
+        title: 'ts',
+        price: 20,
+    },
+];
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQuery',
+    fields: {
+        courses: {
+            type: new GraphQLList(CourseType),
+            resolve: () => {
+                return courses;
+            },
+        },
+    },
+});
+
+const courseSchema = new GraphQLSchema({
+    query: RootQuery,
+});
+
+module.exports = courseSchema;
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const { createHandler } = require('graphql-http/lib/use/http');
+const courseSchema = require('./graphql/course');
 const app = express();
 
+app.all('/graphql', createHandler({ schema: courseSchema }));
 app.listen(4000, () => {
     console.log('app is running on port 4000');
 });
